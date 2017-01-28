4f2e7c22e21c8d37ed064cc7e28947c661e4ef78
ShadowTemplate <taneburgo+shadowtemplate@gmail.com>
2017-01-28 17:23:41 +0100

    - Request skeletons to local-database

--- src/main/java/vlc/pc/dispatcher/RequestDispatcher.java
+++ src/main/java/vlc/pc/dispatcher/RequestDispatcher.java
@@ -1,37 +1,49 @@
 package vlc.pc.dispatcher;
 
 import com.google.api.server.spi.ServiceException;
-import vlc.common.to.AppointmentTO;
-import vlc.common.to.UserActivityTO;
+import vlc.common.util.transformer.SoapAppointmentTransformer;
+import vlc.common.util.transformer.SoapUserActivityTransformer;
+import vlc.ldb.soap.LocalDatabase;
+import vlc.ldb.soap.LocalDatabaseService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class RequestDispatcher {
 
-    public static List<AppointmentTO> listAppointments(Integer telegramId) throws ServiceException {
-        // TODO
-        List<AppointmentTO> appointments = new ArrayList<>();
-        appointments.add(new AppointmentTO(1, telegramId, 123, "date1", "location1"));
-        appointments.add(new AppointmentTO(2, telegramId, 456, "date2", "location2"));
-        return appointments;
+    public static List<vlc.common.to.AppointmentTO> listAppointments(Integer telegramId) throws ServiceException {
+        LocalDatabase service = new LocalDatabaseService().getLocalDatabaseImplPort();
+        List<vlc.ldb.soap.AppointmentTO> appointmentsSoap = service.listAppointments();
+        List<vlc.common.to.AppointmentTO> appointmentsTO = new ArrayList<>();
+        SoapAppointmentTransformer transformer = new SoapAppointmentTransformer();
+        for (vlc.ldb.soap.AppointmentTO appointmentSoap : appointmentsSoap) { // TODO filter by telegramId
+            appointmentsTO.add(transformer.toTO(appointmentSoap));
+        }
+        return appointmentsTO;
     }
 
-    public static List<UserActivityTO> listActivities(Integer telegramId) throws ServiceException {
-        // TODO
-        List<UserActivityTO> activities = new ArrayList<>();
-        activities.add(new UserActivityTO(1, 1, 3, "details1", 100L, 10L, "tomorrow", false));
-        activities.add(new UserActivityTO(2, 2, 3, "details2", 100L, 5L, "tomorrow", true));
-        return activities;
+    public static List<vlc.common.to.UserActivityTO> listActivities(Integer telegramId) throws ServiceException {
+        LocalDatabase service = new LocalDatabaseService().getLocalDatabaseImplPort();
+        List<vlc.ldb.soap.UserActivityTO> userActivitiesSoap = service.listUserActivities();
+        List<vlc.common.to.UserActivityTO> userActivitiesTO = new ArrayList<>();
+        SoapUserActivityTransformer transformer = new SoapUserActivityTransformer();
+        for (vlc.ldb.soap.UserActivityTO userActivitySoap : userActivitiesSoap) { // TODO filter by telegramId
+            userActivitiesTO.add(transformer.toTO(userActivitySoap));
+        }
+        return userActivitiesTO;
     }
 
-    public static UserActivityTO getActivityProgress(Integer activityId) throws ServiceException {
-        // TODO
-        return new UserActivityTO(4, 2, 3, "details", 50L, 10L, "tomorrow", false);
+    public static vlc.common.to.UserActivityTO getActivityProgress(Integer activityId) throws ServiceException {
+        LocalDatabase service = new LocalDatabaseService().getLocalDatabaseImplPort();
+        vlc.ldb.soap.UserActivityTO userActivitySoap = service.readUserActivity(activityId);
+        SoapUserActivityTransformer transformer = new SoapUserActivityTransformer();
+        return transformer.toTO(userActivitySoap);
     }
 
     public static void updateActivityProgress(Integer telegramId, Integer activityId, Long value)
             throws ServiceException {
+        LocalDatabase service = new LocalDatabaseService().getLocalDatabaseImplPort();
+        //service.updateUserActivity();
         // TODO
     }
 }
